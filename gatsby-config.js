5092ab37c85d6f436ca7c3b0bdb477ac5114c071
timakin <timaki.st@gmail.com>
2020-03-23 01:26:17 +0900

    updated home description

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,10 @@ module.exports = {
     // siteUrl: ``,
     home: {
       title: `Covidoc`,
-      description: `A website to catch up upstream and reliable sources about COVID-19.`,
+      description: `
+Covidoc is a website that allows users to view only links to reliable public and medical documents in order to prevent the information flooding that comes with the COVID-19 epidemic.      
+This website is based on the Google Spreadsheet, so anyone can add a link from that sheet.
+I am publishing this site with the hope that we can make a calm and quick effort to support each other without inadvertently panicking.`,
     },
   },
   plugins: [
